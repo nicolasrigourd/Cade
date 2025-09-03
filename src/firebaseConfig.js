9ba18ded6e189cd908240fc5efdf7647bb299731
nicolasrigourd <nico.rigourd29@gmail.com>
2025-09-02 23:54:19 -0300

    cambio firebase caonfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,16 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Importa getFirestore
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAZX9kyjsLJq-DlB-mTJYXGAd_t5Q4OuvE",
   authDomain: "basecadeteria.firebaseapp.com",
   projectId: "basecadeteria",
-  storageBucket: "basecadeteria.firebasestorage.app",
+  storageBucket: "basecadeteria.appspot.com", // ✅ corregido
   messagingSenderId: "390353313407",
   appId: "1:390353313407:web:a71390e8185b41f16da94a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializa Firestore y exporta la instancia
 export const db = getFirestore(app);
+export const auth = getAuth(app);
